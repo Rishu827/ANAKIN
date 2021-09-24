db91c64fb22a6eb900d8d2d754d43abce5fee0ad
rishu827 <r.singhal.com@gmail.com>
2021-09-24 11:44:02 +0530

    Coordinate Suggester - Screening Task

---
+++ coordinateSuggester.py
@@ -0,0 +1,64 @@
+import re
+import time
+import sys
+import os
+
+''' 
+running command from terminal -
+python coordinateSuggester.py filename filepath
+    filename : required : name of html/text file that contains the source code
+    filepath : optional : path to the file, if not then it will look for the file in existing directory
+'''
+
+latlng_regex = "\"latitude\":-{0,1}\d{1,2}\.\d\d+,\"longitude\":-{0,1}\d{1,3}\.\d\d+"
+latlng_regex = re.compile("(?i)"+latlng_regex)
+
+
+def readFile(fileName):
+    htmlFile = open(os.path.join(fileName),'r',encoding="utf-8")
+
+    sourceCode = ""
+    line = htmlFile.readline()
+    while(line != ""):
+        sourceCode += line
+        line = htmlFile.readline()
+    
+    return sourceCode
+
+
+def getCoordinates(htmlFile):
+    coordinates = latlng_regex.findall(htmlFile)
+    return coordinates
+
+    
+if __name__ == "__main__":
+
+    file_name = ""
+
+    systemArgs = len(sys.argv)
+
+    if(systemArgs == 1):
+        print("Missing at least 1 parameter")
+        quit()
+     
+    elif(systemArgs == 2):
+        file_name = sys.argv[1]
+
+    elif(systemArgs == 3):
+        file_name = sys.argv[1]
+        file_path = sys.argv[2]
+        file_name = os.path.join(file_path,file_name)
+    
+    else:
+        print("More than required parameters given")
+        quit()
+    
+    
+   
+    a = time.time()
+    htmlFile = readFile(file_name)
+    coordinates = getCoordinates(htmlFile)
+    print("Total of {} Coordinates found in {} seconds".format(len(coordinates),time.time()-a))    
+
+    for i in coordinates:
+        print(i)
